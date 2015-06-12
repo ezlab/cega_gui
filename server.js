552ef0f654e85b8e34d178c704c8a4636f5f7f5c
kamushkin <kamushkin@users.noreply.github.com>
2015-06-12 18:19:37 +0200

    add position, element services

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ var express = require('express'),
 	proxy = require('express-http-proxy'),
 	app = express();
 
+var api = function(path){
+	return proxy('everest.unige.ch:8000', {
+		forwardPath: function(req, res){
+			return path + String(req.url).replace(/^\//, '');
+		}
+	});
+};
+
 var file = function(path, type){
 	return function(req, res){
 		res.sendFile(__dirname + path, type ? {headers: {'Content-Type': type}} : {});
@@ -10,6 +18,10 @@ var file = function(path, type){
 }
 
 app.use('/static', express.static('static'));
+
+app.use('/position', api('/position'));
+app.use('/element', api('/element'));
+
 app.get('/', file('/index.html'));
 
 app.listen(process.env.PORT || 80);
