d6429bf276f0ebcba219b0dbc53738efdeef4283
kamushkin <kamushkin@users.noreply.github.com>
2015-06-18 01:38:18 +0200

    add MSA widget

--- static/js/elements.js
+++ static/js/elements.js
@@ -1,6 +1,14 @@
 
 (function(){
 
+	var template;
+
+	app.on('init', function(){
+		return $.get('static/templates/msa.html').then(function(source){
+			template = Handlebars.compile(source);
+		});
+	});
+
 	function toggleRow(index){
 
 		var box = $('#position-' + index),
@@ -27,14 +35,40 @@
 
 		var elements = app.load('/element', values);
 
-		app.render('elements.html', elements).then(function(html){
+		var ready = app.render('elements.html', {index:index}).then(function(html){
 			box.html(html);
+		});
+
+		$.when(elements, ready).then(function(res){
+
+			var i, seqs = [], a = res.data;
+
+			for(i=0; i<a.length; i++){
+				seqs[i] = {
+					id: 'r' + i,
+					labels: template(a[i]),
+					name: a[i].spc,
+					seq: a[i].seq
+				};
+			}
+
+			var opt = {
+					el: document.getElementById('msa' + index),
+					seqs : seqs,
+					vis: {labelId: false},
+					zoomer: {labelNameLength: 420}
+				},
+
+				m = new msa.msa(opt);
+
+			m.render();
+
 		});
 	}
 
 
 	function init(){
-		$('#content').on('click', '.s-position-row', function(event){
+		$('#content').on('click', '.s-position-cells', function(event){
 			toggleRow(event.currentTarget.getAttribute('data-id'));
 		});
 	}
@@ -44,3 +78,16 @@
 
 })();
 
+
+(function(){
+
+	// LabelView
+	require(155).prototype.render = function() {
+
+		this.el.setAttribute("class", "biojs_msa_labels");
+		this.el.style.width = (this.g.zoomer.getLabelWidth()) + "px";
+		this.el.innerHTML = this.model.attributes.labels;
+		return this;
+	};
+
+})();
