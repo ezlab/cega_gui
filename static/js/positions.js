29084fa9526e58000fa1570cb12f5fe0a1ddd3c0
kamushkin <kamushkin@users.noreply.github.com>
2015-06-19 18:03:40 +0200

    Implement incremental loading on verticall scroll in position table. Close #8.

--- static/js/positions.js
+++ static/js/positions.js
@@ -1,27 +1,65 @@
 
 (function(){
 
+	var rows = [],
+		rendered = 0,
+		total = 0,
+		skip = 0,
+		limit = 100;
+
+
 	app.helper('subtract', function(v1, v2){
 		return Number(v2) - Number(v1);
 	});
 
 
-	function savePositions(response){
-		app.set('positions', response.data);
-		return response;
+	function appendRows(content){
+		$('#positions').append(content);
 	}
 
 
-	function replaceContent(content){
-		$('#content').html(content);
-	}
+	function updateDisplay(){
 
+		var content = $('#content')[0];
 
-	function requestPositions(params, state){
+		if (rendered < total && content.scrollHeight - content.offsetHeight - content.scrollTop < 100){
 
-		if (!params.clade || !params.species){
-			return;
+			app.render('rows.html', rows.slice(rendered, rendered + 50)).then(appendRows);
+
+			rendered += 50;
+
+			if (rows.length < total && rows.length - rendered < 50){
+				requestPositions();
+			}
 		}
+	}
+
+
+	function updatePositions(content){
+		$('#content').html(content);
+		updateDisplay();
+	}
+
+
+	function savePositions(response){
+
+		if (response.data && response.data.length){
+			rows = rows.concat(response.data);
+		}
+
+		if (!total){
+			total = response.count;
+			app.render('positions.html', response).then(updatePositions);
+		}
+		else {
+			updateDisplay();
+		}
+	}
+
+
+	function requestPositions(){
+
+		var state = app.state();
 
 		var values = {
 			clade: state.clade,
@@ -29,15 +67,44 @@
 			request: state.request,
 			length: state.length,
 			score: state.score,
-			status: state.status
+			status: state.status,
+			sort: state.sort,
+			skip: skip,
+			limit: limit
 		};
 
-		var positions = app.load('/position', values).then(savePositions);
+		skip += limit;
 
-		app.render('positions.html', positions).then(replaceContent);
+		app.load('/position', values).then(savePositions);
 	}
 
-	app.on('navigate', requestPositions);
+
+	app.on('navigate', function(params){
+
+		if (!params.clade || !params.species){
+			return;
+		}
+
+		rows = [];
+		rendered = 0,
+		total = 0;
+		skip = 0;
+
+		$('#content').html('<div class="s-loading">Loading..</div>');
+
+		requestPositions();
+	});
+
+
+	app.on('init', function(){
+
+		$('#content').on('scroll', function(){
+
+			if ($('#positions').length){
+				updateDisplay();
+			}
+		});
+	});
 
 })();
 
